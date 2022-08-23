e5eef95b157be7512d5021c39ed7b5574d42dffd
haemin9014 <leehaemin9014@gmail.com>
2022-08-24 05:23:58 +0900

    setting button

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -2,14 +2,17 @@
 
 //since it is class use .
 //if it is id use #
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number!';
+// document.querySelector('.message').textContent = 'Correct Number!';
 
-console.log(document.querySelector('.message').textContent); //printing out changed.
+// console.log(document.querySelector('.message').textContent); //printing out changed.
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-//when we use input we use value
-document.querySelector('.guess').value = 23;
+// //when we use input we use value
+// document.querySelector('.guess').value = 23;
+
+//setting the button
+document.querySelector('btn-check').addEventListener();
