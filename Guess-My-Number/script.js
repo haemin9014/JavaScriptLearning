6256043cd5e9631c92413c221351702e909fb9a3
haemin9014 <leehaemin9014@gmail.com>
2022-08-24 08:17:19 +0900

    setting basic logic

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,18 +1,29 @@
 'use strict';
-
-//since it is class use .
-//if it is id use #
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = 'Correct Number!';
-
-// console.log(document.querySelector('.message').textContent); //printing out changed.
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// //when we use input we use value
-// document.querySelector('.guess').value = 23;
-
 //setting the button
-document.querySelector('btn-check').addEventListener();
+
+const number = Math.trunc(Math.random() * 20) + 1;
+console.log(number);
+document.querySelector('.check').addEventListener('click', function () {
+  //Number is built in function that makes value to Integer
+  const guess = Number(document.querySelector('.guess').value);
+  let wrong = Number(document.querySelector('.score').textContent);
+  console.log(guess);
+  if (wrong > 0) {
+    if (!guess) {
+      document.querySelector('.message').textContent = 'No number!';
+    } else if (guess < number) {
+      document.querySelector('.message').textContent = 'Higher!';
+      wrong--;
+    } else if (guess > number) {
+      document.querySelector('.message').textContent = 'Lower!';
+      wrong--;
+    } else {
+      document.querySelector('.message').textContent =
+        'Congrats! you found a number!';
+      document.querySelector('.number').textContent = number;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'you lost the game!';
+  }
+  document.querySelector('.score').textContent = wrong;
+});
